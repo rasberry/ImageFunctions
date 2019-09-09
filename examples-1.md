a006baaadcc4ff213dfe2c0a4d2569f006983e26
Rasberry <rasberry@hotmail.com>
2019-09-08 22:41:52 -0600

    added sample images and generator script

---
+++ examples-1.md
@@ -0,0 +1,10 @@
+# PixelateDetails #
+| Image   | Default | -p | -s 3 | -r 3 |
+|---------|---------|---|---|---|
+|boy     |![boy-1](img-1/boy-1.png "boy-1")|![boy-2](img-1/boy-2.png "boy-2")|![boy-3](img-1/boy-3.png "boy-3")|![boy-4](img-1/boy-4.png "boy-4")
+|building|![building-1](img-1/building-1.png "building-1")|![building-2](img-1/building-2.png "building-2")|![building-3](img-1/building-3.png "building-3")|![building-4](img-1/building-4.png "building-4")
+|cats    |![cats-1](img-1/cats-1.png "cats-1")|![cats-2](img-1/cats-2.png "cats-2")|![cats-3](img-1/cats-3.png "cats-3")|![cats-4](img-1/cats-4.png "cats-4")
+|cloud   |![cloud-1](img-1/cloud-1.png "cloud-1")|![cloud-2](img-1/cloud-2.png "cloud-2")|![cloud-3](img-1/cloud-3.png "cloud-3")|![cloud-4](img-1/cloud-4.png "cloud-4")
+|cookie  |![cookie-1](img-1/cookie-1.png "cookie-1")|![cookie-2](img-1/cookie-2.png "cookie-2")|![cookie-3](img-1/cookie-3.png "cookie-3")|![cookie-4](img-1/cookie-4.png "cookie-4")
+|creek   |![creek-1](img-1/creek-1.png "creek-1")|![creek-2](img-1/creek-2.png "creek-2")|![creek-3](img-1/creek-3.png "creek-3")|![creek-4](img-1/creek-4.png "creek-4")
+|flower  |![flower-1](img-1/flower-1.png "flower-1")|![flower-2](img-1/flower-2.png "flower-2")|![flower-3](img-1/flower-3.png "flower-3")|![flower-4](img-1/flower-4.png "flower-4")
---
+++ make.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/41731740/how-to-pass-an-array-into-a-bash-function
+
+# im="/mnt/d/Software/visual/ImageMagickQ16HDRI/magick.exe"
+dotnet="/mnt/c/Program Files/dotnet/dotnet.exe"
+src="img/*"
+
+function pad() {
+	printf "%-$1s" "$2"
+}
+
+function maxlen() {
+	local -n maxlen_r="$1"
+	maxlen=0
+	for n in "${maxlen_r[@]}"; do
+		len="${#n}"
+		if [ "$len" -gt "$maxlen" ]; then maxlen="$len"; fi
+	done
+	echo "$maxlen"
+}
+
+# optionsarray name outfolder opt...
+function md() {
+	echo "# $2 #"
+	s=4; e="$#"
+	tit="| Image   | Default |"
+	sep="|---------|---------|"
+	while [ $s -le $e ]; do
+		tit="$tit ${!s} |"
+		sep="$sep---|"
+		s=$((s + 1))
+	done
+	echo "$tit"
+	echo "$sep"
+	
+	local -n md_r="$1"
+	max=$(maxlen "md_r")
+	for n in "${md_r[@]}"; do
+		p=$(pad "$max" "$n")
+		row="|$p"
+		s=3;
+		while [ $s -le $e ]; do
+			i=$((s - 2))
+			row="$row|![$n-$i]($3/$n-$i.png \"$n-$i\")"
+			s=$((s + 1))
+		done
+		echo "$row"
+	done
+	
+}
+
+# imagearray outfolder index action options
+function makeset() {
+	local -n makeset_r="$1"
+	if [ ! -d "$2" ]; then mkdir "$2"; fi
+	for n in "${makeset_r[@]}"; do
+		"$dotnet" run -p "../src" "--no-build" -- "$4" $5 "img/$n.png" "$2/$n-$3.png"
+	done
+}
+
+# imagearray optionsarray action name outfolder
+function makepermute() {
+	local -n makepermute_r="$2"
+
+	#default
+	makeset "$1" "$5" "1" "$3"
+	#with options
+	i=2
+	for o in "${makepermute_r[@]}"; do
+		makeset "$1" "$5" "$i" "$3" "$o"
+		i=$((i + 1))
+	done
+	md "$1" "$4" "$5" "${makepermute_r[@]}"
+}
+
+function make-1() {
+	make1_r=("boy" "building" "cats" "cloud" "cookie" "creek" "flower")
+	opts_r=("-p" "-s 3" "-r 3")
+	makepermute "make1_r" "opts_r" "1" "PixelateDetails" "img-1"
+}
+
+function make-2() {
+	make2_r=("fractal" "handle" "harddrive" "lego" "pool" "rainbow" "road")
+	opts_r=("-g" "-a")
+	makepermute "make2_r" "opts_r" "2" "Derivatives" "img-2"
+}
+
+# make-1
+make-2
+
+# boy
+# building
+# cats
+# cloud
+# cookie
+# creek
+# flower
+# fractal
+# handle
+# harddrive
+# lego
+# pool
+# rainbow
+# road
+# rock
+# scorpius
+# shack
+# shell
+# skull
+# spider
+# toes
+# zebra
