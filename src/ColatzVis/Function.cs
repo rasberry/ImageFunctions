1e7540132c9878c35d3942d909efd83b0bddf334
rasberry <rasberry@hotmail.com>
2020-06-16 20:46:46 -0600

    abstracted collatz (not tested)

--- src/ColatzVis/Function.cs
+++ src/ColatzVis/Function.cs
@@ -1,12 +1,60 @@
 using System;
 using System.Text;
+using System.Drawing;
 using ImageFunctions.Helpers;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing.Processors;
-using SixLabors.Primitives;
 
 namespace ImageFunctions.ColatzVis
 {
+	public class Function : IFAbstractFunction, IFGenerator
+	{
+		public Size StartingSize { get {
+			return new Size(256,256);
+		}}
+
+		public override bool ParseArgs(string[] args)
+		{
+			int len = args.Length;
+			for(int a=0; a<len; a++)
+			{
+				string curr = args[a];
+				if (false) {
+				}
+				else if (OutImage == null) {
+					OutImage = curr;
+				}
+			}
+
+			if (String.IsNullOrEmpty(OutImage)) {
+				OutImage = OptionsHelpers.CreateOutputFileName(nameof(ColatzVis));
+			}
+
+			return true;
+		}
+
+		public override void Usage(StringBuilder sb)
+		{
+			return; //TODO disabled for now
+			
+			#if false
+			string name = OptionsHelpers.FunctionName(Activity.ColatzVis);
+			sb.AppendLine();
+			sb.AppendLine(name + " [options] [output image]");
+			sb.AppendLine(" TODO Does something ");
+			//sb.AppendLine(" -g (name)                   Choose which graphic to create");
+			//sb.AppendLine(" -bg (color)                 Change Background color (default transparent)");
+			sb.AppendLine();
+			#endif
+		}
+
+		protected override IFAbstractProcessor CreateProcessor()
+		{
+			return new Processor();
+		}
+
+		Options O = new Options();
+	}
+
+	#if false
 	public class Function : AbstractFunction, IGenerator
 	{
 		public Size StartingSize { get {
@@ -65,4 +113,5 @@ namespace ImageFunctions.ColatzVis
 
 		Options O = new Options();
 	}
+	#endif
 }
