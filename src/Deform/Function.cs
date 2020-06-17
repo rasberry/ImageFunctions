1d3bc32b3a4c6edd6bda2e80d90617b3b4e45fa2
rasberry <rasberry@hotmail.com>
2020-06-16 20:33:19 -0600

    abstracted deform (not tested)

--- src/Deform/Function.cs
+++ src/Deform/Function.cs
@@ -1,18 +1,88 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Drawing;
 using ImageFunctions.Helpers;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Processing.Processors;
-using SixLabors.ImageSharp.Processing.Processors.Transforms;
-using SixLabors.Primitives;
 
 namespace ImageFunctions.Deform
 {
+	public class Function : IFAbstractFunction, IFHasResampler
+	{
+		public override bool ParseArgs(string[] args)
+		{
+			var p = new Params(args);
+			var pcp = p.Default("-cp",out double ppx, out double ppy,
+				tpar:OptionsHelpers.ParseNumberPercent,
+				upar:OptionsHelpers.ParseNumberPercent
+			);
+			if (pcp.IsInvalid()) {
+				return false;
+			}
+			else if(pcp.IsGood()) {
+				O.CenterPp = new PointF((float)ppx,(float)ppy);
+			}
+
+			var pcx = p.Default("-cx",out int cx, out int cy);
+			if (pcx.IsInvalid()) {
+				return false;
+			}
+			else if (pcx.IsGood()) {
+				O.CenterPx = new Point(cx,cy);
+			}
+
+			// -cp and -cx are either/or options so choose a default if neither were specified
+			if (O.CenterPx == null && O.CenterPp == null) {
+				O.CenterPp = new PointF(0.5f,0.5f);
+			}
+
+			if (p.Default("-e",out O.Power,2.0).IsInvalid()) {
+				return false;
+			}
+			if (p.Default("-m",out O.WhichMode,Mode.Polynomial).IsInvalid()) {
+				return false;
+			}
+			if (p.DefaultSampler(out O.Sampler,Registry.DefaultIFResampler).IsInvalid()) {
+				return false;
+			}
+
+			if (p.ExpectFile(out InImage,"input image").IsBad()) {
+				return false;
+			}
+			if (p.DefaultFile(out OutImage,InImage).IsInvalid()) {
+				return false;
+			}
+
+			return true;
+		}
+
+		public override void Usage(StringBuilder sb)
+		{
+			string name = OptionsHelpers.FunctionName(Activity.Deform);
+			sb.WL();
+			sb.WL(0,name + " [options] (input image) [output image]");
+			sb.WL(1,"Warps an image using a mapping function");
+			sb.WL(1,"-cc (number) (number)"      ,"Coordinates of center in pixels");
+			sb.WL(1,"-cp (number)[%] (number)[%]","Coordinates of center by proportion (default 50% 50%)");
+			sb.WL(1,"-e (number)"                ,"(e) Power Exponent (default 2.0)");
+			sb.WL(1,"-m (mode)"                  ,"Choose mode (default Polynomial)");
+			sb.SamplerHelpLine();
+			sb.WL();
+			sb.WL(1,"Available Modes");
+			sb.WL(1,"1. Polynomial","x^e/w, y^e/h");
+			sb.WL(1,"2. Inverted"  ,"n/x, n/y; n = (x^e + y^e)");
+		}
+
+		protected override IFAbstractProcessor CreateProcessor()
+		{
+			return new Processor();
+		}
+
+		Options O = new Options();
+		public IFResampler Sampler { get { return O.Sampler; }}
+
+	}
+
+	#if false
 	public class Function : AbstractFunction, IHasResampler
 	{
 		public override IImageProcessor<TPixel> CreatePixelSpecificProcessor<TPixel>(Image<TPixel> source, Rectangle sourceRectangle)
@@ -96,4 +166,5 @@ namespace ImageFunctions.Deform
 		Options O = new Options();
 		public IResampler Sampler { get { return O.Sampler; }}
 	}
+	#endif
 }
--- src/Deform/Processor.cs
+++ src/Deform/Processor.cs
@@ -1,13 +1,83 @@
-using ImageFunctions.Helpers;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Advanced;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing.Processors.Transforms;
-using SixLabors.Primitives;
 using System;
+using System.Drawing;
+using ImageFunctions.Helpers;
 
 namespace ImageFunctions.Deform
 {
+	public class Processor : IFAbstractProcessor
+	{
+		public Options O = null;
+
+		public override void Apply()
+		{
+			var Iis = Engines.Engine.GetConfig();
+			var frame = Source;
+			var rect = Bounds;
+
+			using (var progress = new ProgressBar())
+			using (var canvas = Iis.NewImage(rect.Width,rect.Height))
+			{
+				double ccx,ccy;
+				if (O.CenterPx != null) {
+					ccx = O.CenterPx.Value.X;
+					ccy = O.CenterPx.Value.Y;
+				}
+				else {
+					ccx = frame.Width * (O.CenterPp == null ? 0.5 : O.CenterPp.Value.X);
+					ccy = frame.Height * (O.CenterPp == null ? 0.5 : O.CenterPp.Value.Y);
+				}
+
+				MoreHelpers.ThreadPixels(rect, MaxDegreeOfParallelism, (x,y) => {
+					int cy = y - rect.Top;
+					int cx = x - rect.Left;
+					IFColor nc = ProjectPixel(frame,x,y,ccx,ccy,O.Power);
+					canvas[cx,cy] = nc;
+				},progress);
+
+				frame.BlitImage(canvas,rect);
+			}
+		}
+
+		IFColor ProjectPixel(IFImage frame,double x, double y,double ccx, double ccy,double exp)
+		{
+			double qw = x <= ccx ? ccx : frame.Width - ccx;
+			double qh = y <= ccy ? ccy : frame.Height - ccy;
+
+			x -= ccx; y -= ccy;
+			double px = 0.0, py = 0.0;
+
+			switch(O.WhichMode)
+			{
+			case Mode.Polynomial: {
+				//solve(w^q/n = w,n) : n = w^(q-1)
+				double dx = Math.Pow(Math.Abs(qw),exp - 1.0);
+				double dy = Math.Pow(Math.Abs(qh),exp - 1.0);
+				px = Math.Sign(x) * Math.Pow(Math.Abs(x),exp) / dx;
+				py = Math.Sign(y) * Math.Pow(Math.Abs(y),exp) / dy;
+			}; break;
+			case Mode.Inverted: {
+				double ax = Math.Pow(Math.Abs(x),exp);
+				double ay = Math.Pow(Math.Abs(y),exp);
+				double aw = Math.Pow(Math.Abs(qw),exp);
+				double ah = Math.Pow(Math.Abs(qh),exp);
+				double num = ax + ay;
+				// solve(((w^e+h^e)/w)*n=w,n);
+				double dx = (qw * qw)/(aw+ah);
+				double dy = (qh * qh)/(ah+aw);
+				px = num / x * dx;
+				py = num / y * dy;
+			}; break;
+			}
+
+			px += ccx; py += ccy;
+
+			return ImageHelpers.Sample(frame,px,py,O.Sampler);
+		}
+
+		public override void Dispose() {}
+	}
+
+	#if false
 	public class Processor<TPixel> : AbstractProcessor<TPixel>
 		where TPixel : struct, IPixel<TPixel>
 	{
@@ -76,4 +146,5 @@ namespace ImageFunctions.Deform
 			return ImageHelpers.Sample(frame,px,py,O.Sampler);
 		}
 	}
+	#endif
 }
