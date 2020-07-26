838067c9d62f2a538251070b2c17fc9840d5a96d
rasberry <rasberry@hotmail.com>
2020-07-25 23:22:08 -0600

    saving progress

---
+++ src/Maze/Prims.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ImageFunctions.Helpers;
+
+namespace ImageFunctions.Maze
+{
+	public class Prims : IMaze
+	{
+		public Options O { get; set; }
+		public Action<int,int,PickWall> DrawCell { get; set; }
+		public Func<int,int,PickWall,bool> IsBlocked { get; set; }
+		public int CellsWide { get; set; }
+		public int CellsHigh { get; set; }
+
+		struct Cell
+		{
+			public Cell(int x,int y,PickWall w) {
+				P = new Point(x,y);
+				W = w;
+			}
+			public Point P;
+			public PickWall W;
+
+			public override string ToString() {
+				return $"[{P.X},{P.Y}] {W}";
+
+			}
+		}
+
+		Random Rnd = null;
+		List<Cell> Walls = new List<Cell>();
+
+		public void DrawMaze(ProgressBar prog)
+		{
+			Rnd = O.RndSeed.HasValue ? new Random(O.RndSeed.Value) : new Random();
+
+			Point first = FindCell();
+			AddWallsForCell(first.X,first.Y);
+			DrawCell(first.X,first.Y,PickWall.None);
+			
+			//Log.Debug($"size {CellsWide},{CellsHigh}");
+			int TCells = CellsWide * CellsHigh;
+			int CellCount = 0;
+
+			while(Walls.Count > 0) {
+				//PrintQueue();
+				Cell c = RemoveRandomWall();
+				//Log.Debug($"C = {c}");
+				if (MarkCellsForWall(c)) {
+					CellCount++;
+					prog.Report((double)CellCount / TCells);
+				}
+				//if (CellCount > 4 * TCells) { break; } //TODO remove this when it's working
+			}
+		}
+
+		void PrintQueue()
+		{
+			var sb = new StringBuilder();
+			int i = 0;
+			foreach(var c in Walls) {
+				sb.Append($" #{i++} {c}");
+			}
+			Log.Debug(sb.ToString());
+		}
+
+		Point FindCell()
+		{
+			return new Point(
+				Rnd.Next(0,CellsWide),
+				Rnd.Next(0,CellsHigh)
+			);
+		}
+
+		void AddWallsForCell(int x,int y)
+		{
+			bool isWall = IsBlocked(x,y,PickWall.None);
+			if (!isWall) { return; }
+			int h = CellsHigh;
+			int w = CellsWide;
+
+			if (y > 0) { Walls.Add(new Cell(x,y,PickWall.N)); }
+			if (y < h) { Walls.Add(new Cell(x,y,PickWall.S)); }
+			if (x > 0) { Walls.Add(new Cell(x,y,PickWall.W)); }
+			if (x < w) { Walls.Add(new Cell(x,y,PickWall.E)); }
+		}
+
+		Cell RemoveRandomWall()
+		{
+			//doing a switch and remove so we don't have to shift everything after the chosen wall O(n)
+			int len = Walls.Count;
+			int which = Rnd.Next(0,len);
+			Cell c = Walls[which];
+			Cell e = Walls[len - 1];
+			Walls[which] = e; //switch
+			Walls.RemoveAt(len - 1); //remove last element (cheap O(1))
+			return c;
+		}
+
+		bool MarkCellsForWall(Cell c)
+		{
+			Cell q = default(Cell); bool wasSet = true;
+			switch(c.W) {
+				case PickWall.N: q = new Cell(c.P.X,c.P.Y-1,PickWall.None); break;
+				case PickWall.E: q = new Cell(c.P.X+1,c.P.Y,PickWall.None); break;
+				case PickWall.S: q = new Cell(c.P.X,c.P.Y+1,PickWall.None); break;
+				case PickWall.W: q = new Cell(c.P.X-1,c.P.Y,PickWall.None); break;
+				default: wasSet = false; break; //q was not set
+			}
+			//return if out of bounds or if target cell is already open
+			if (!wasSet || !IsBlocked(q.P.X,q.P.Y,q.W)) {
+				//Log.Debug($"isCell {q}");
+				return false;
+			}
+
+			AddWallsForCell(q.P.X,q.P.Y);
+			DrawCell(q.P.X, q.P.Y, c.W);
+			//Log.Debug($"hole {c} {q} [{c.W}]");
+			return true;
+		}
+	}
+}
--- src/Maze/Processor.cs
+++ src/Maze/Processor.cs
@@ -3,6 +3,20 @@ using ImageFunctions.Helpers;
 
 namespace ImageFunctions.Maze
 {
+	public interface IMaze {
+		Options O { get; set; }
+		void DrawMaze(ProgressBar prog);
+		Action<int,int,PickWall> DrawCell { get; set; }
+		Func<int,int,PickWall,bool> IsBlocked { get; set; }
+		int CellsWide { get; set; }
+		int CellsHigh { get; set; }
+	}
+
+	[Flags]
+	public enum PickWall {
+		None = 0, N = 1, E = 2, S = 4, W = 8
+	}
+
 	public class Processor : AbstractProcessor
 	{
 		public Options O = null;
@@ -10,28 +24,36 @@ namespace ImageFunctions.Maze
 		//TODO implement using Bounds
 		public override void Apply()
 		{
-			ImageHelpers.FillWithColor(Source,O.WallColor);
+			IMaze maze = null;
+			switch(O.Which) {
+			case PickMaze.Eller: maze = new Ellers(); break;
+			case PickMaze.Prims: maze = new Prims(); break;
+			}
+			//Log.Debug("maze :"+O.Which);
 
-			Rnd = new Random(); //TODO add option for seed
-			using (var progress = new ProgressBar())
-			{
-				DrawEllers(Source, progress);
+			maze.CellsWide = Source.Width / 2;
+			maze.CellsHigh = Source.Width / 2;
+			maze.O = O;
+			maze.DrawCell = DrawCell;
+			maze.IsBlocked = IsBlocked;
+
+			ImageHelpers.FillWithColor(Source,O.WallColor);
+			using (var progress = new ProgressBar()) {
+				maze.DrawMaze(progress);
 			}
 		}
 
 		public override void Dispose() {}
 
-		static Random Rnd = null;
-
-		[Flags]
-		enum PickWall {
-			None = 0, N = 1, E = 2, S = 4, W = 8
-		}
-
-		void DrawCell(IImage img, int cx,int cy, PickWall removeWalls)
+		void DrawCell(int cx,int cy, PickWall removeWalls)
 		{
+			var img = Source;
 			int x = cx * 2 + 1;
 			int y = cy * 2 + 1;
+			if (x < 0 || x >= img.Width || y < 0 || y >= img.Height) {
+				return;
+			}
+			//Log.Debug($"Draw [{cx},{cy}] {removeWalls}");
 			img[x,y] = O.CellColor;
 			if (removeWalls.HasFlag(PickWall.N) && y > 0) {
 				img[x,y-1] = O.CellColor;
@@ -47,107 +69,24 @@ namespace ImageFunctions.Maze
 			}
 		}
 
-		//http://www.neocomputer.org/projects/eller.html
-		//http://weblog.jamisbuck.org/2010/12/29/maze-generation-eller-s-algorithm
-		void DrawEllers(IImage img, ProgressBar prog)
+		bool IsBlocked(int cx,int cy, PickWall which)
 		{
-			int width = img.Width / 2;
-			int lastRow = img.Height / 2 - 1;
-			int[] row0 = new int[width];
-			int[] row1 = new int[width];
-			int[] cRow = row0; //current row
-			int[] nRow = row1; //next row
-			int nextSet = width+1; //set to the value after Step 1
-			int rowNum = 0; //row #
-
-			//Step 1: Initialize the cells of the first row to each exist in their own set.
-			for(int c=0; c<width; c++) {
-				cRow[c] = c + 1; //zero means not in a set
+			var img = Source;
+			int x = cx * 2 + 1;
+			int y = cy * 2 + 1;
+			if (x < 0 || x >= img.Width || y < 0 || y >= img.Height) {
+				return false;
 			}
 
-			while(rowNum < lastRow)
-			{
-				prog.Report((double)rowNum / lastRow);
-
-				//Step 2: Now, randomly join adjacent cells, but only if they are not in the same set.
-				// When joining adjacent cells, merge the cells of both sets into a single set, indicating
-				// that all cells in both sets are now connected (there is a path that connects any two
-				// cells in the set).
-				for (int c = 1; c < width; c++) {
-					bool merge = Rnd.RandomChoice();
-					if (merge) {
-						cRow[c] = cRow[c - 1];
-					}
-				}
-
-				//Step 3: For each set, randomly create vertical connections downward to the next row.
-				// Each remaining set must have at least one vertical connection. The cells in the next
-				// row thus connected must share the set of the cell above them.
-				int currSet = cRow[0]; //current set #
-				bool hasConnection = false;
-				for (int c = 0; c <= width; c++) {
-					bool wasLast = c == width || cRow[c] != currSet;
-					if (wasLast) {
-						if (!hasConnection) {
-							nRow[c - 1] = cRow[c - 1];
-						}
-						hasConnection = false;
-						if (c == width) { break; }
-						currSet = cRow[c];
-					}
-
-					bool connect = Rnd.RandomChoice();
-					if (connect) {
-						nRow[c] = cRow[c];
-						hasConnection = true;
-					}
-				}
-
-				//Step 4: Flesh out the next row by putting any remaining cells into their own sets.
-				for (int c = 0; c < width; c++) {
-					if (nRow[c] == 0) { //zero means not in a set
-						nRow[c] = nextSet;
-						nextSet++;
-						if (nextSet >= int.MaxValue) { nextSet = 1; }
-					}
-				}
-
-				//draw Here
-				
-				EllersDrawRow(img, width, cRow, nRow, rowNum);
-
-				// Step 5: Repeat until the last row is reached.
-				rowNum++;
-				(cRow, nRow) = (nRow, cRow); //swap rows
-				for (int c = 0; c < width; c++) { nRow[c] = 0; } //clear next row
+			switch(which) {
+				case PickWall.None: return img[x,y].Equals(O.WallColor);
+				case PickWall.N: return y > 0              && img[x,y-1].Equals(O.WallColor);
+				case PickWall.E: return x < img.Width - 1  && img[x+1,y].Equals(O.WallColor);
+				case PickWall.S: return y < img.Height - 1 && img[x,y+1].Equals(O.WallColor);
+				case PickWall.W: return x > 0              && img[x-1,y].Equals(O.WallColor);
 			}
 
-			//Step 6: For the last row, join all adjacent cells that do not share a set,
-			// and omit the vertical connections, and you’re done!
-			for (int c=1; c<width; c++) {
-				if (cRow[c] != cRow[c-1]) {
-					cRow[c] = cRow[c-1];
-				}
-			}
-
-			//draw last row
-			EllersDrawRow(img, width, cRow, null, rowNum);
-		}
-
-		void EllersDrawRow(IImage img, int width, int[] cRow, int[] nRow, int rowNum)
-		{
-			PickWall w = PickWall.None;
-
-			for (int c = 0; c < width; c++) {
-				int nc = c == 0 ? c + 1 : c - 1;
-				if (cRow[nc] == cRow[c]) {
-					w |= PickWall.W;
-					if (nRow != null && cRow[c] == nRow[c]) {
-						w |= PickWall.S;
-					}
-					DrawCell(img, c, rowNum, w);
-				}
-			}
+			return false;
 		}
 
 	}
