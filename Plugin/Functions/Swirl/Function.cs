2e6b8e4bda8f05b4a4fc62a1faae4524a3a707a7
rasberry <rasberry@hotmail.com>
2023-10-01 17:51:47 -0600

    added swirl function

---
+++ Plugin/Functions/Swirl/Function.cs
@@ -0,0 +1,89 @@
+using System.Drawing;
+using ImageFunctions.Core;
+using Rasberry.Cli;
+using O = ImageFunctions.Plugin.Functions.Swirl.Options;
+
+namespace ImageFunctions.Plugin.Functions.Swirl;
+
+[InternalRegisterFunction(nameof(Swirl))]
+public class Function : IFunction
+{
+	public void Usage(StringBuilder sb)
+	{
+		O.Usage(sb);
+	}
+
+	public bool Run(IRegister register, ILayers layers, string[] args)
+	{
+		if (layers == null) {
+			throw Squeal.ArgumentNull(nameof(layers));
+		}
+		if (!O.ParseArgs(args, register)) {
+			return false;
+		}
+		if (layers.Count < 1) {
+			Tell.LayerMustHaveAtLeast();
+			return false;
+		}
+		var source = layers.Last();
+
+		double swirlRadius;
+		double swirlTwists = O.Rotations;
+		double swirlx, swirly;
+		Rectangle rect = source.Bounds();
+
+		if (O.RadiusPx != null) {
+			swirlRadius = O.RadiusPx.Value;
+		}
+		else {
+			double m = O.RadiusPp != null ? O.RadiusPp.Value : 0.9;
+			swirlRadius = m * Math.Min(rect.Width,rect.Height);
+		}
+
+		if (O.CenterPx != null) {
+			swirlx = O.CenterPx.Value.X;
+			swirly = O.CenterPx.Value.Y;
+		}
+		else {
+			double px = O.CenterPp != null ? O.CenterPp.Value.X : 0.5;
+			double py = O.CenterPp != null ? O.CenterPp.Value.Y : 0.5;
+			swirlx = rect.Width * px + rect.Left;
+			swirly = rect.Height * py + rect.Top;
+		}
+
+		using var progress = new ProgressBar();
+		using var canvas = layers.NewCanvasFromLayers();
+
+		Tools.ThreadPixels(rect, (x,y) => {
+			int cy = y - rect.Top;
+			int cx = x - rect.Left;
+			ColorRGBA nc = SwirlPixel(source,x,y,swirlx,swirly,swirlRadius,swirlTwists);
+			canvas[cx,cy] = nc;
+		},progress);
+
+		source.CopyFrom(canvas,rect);
+		return true;
+	}
+
+	ColorRGBA SwirlPixel(ICanvas frame,
+		double x, double y, double swirlx, double swirly,
+		double swirlRadius, double swirlTwists)
+	{
+		double pixelx = x - swirlx;
+		double pixely = y - swirly;
+		double pixelDist = O.Metric.Value.Measure(x,y,swirlx,swirly);
+		double swirlAmount = 1.0 - (pixelDist / swirlRadius);
+
+		if (swirlAmount > 0.0) {
+			double twistAngle = swirlTwists * swirlAmount * Math.PI * 2;
+			if (!O.CounterClockwise) { twistAngle *= -1.0; }
+			double pixelAng = Math.Atan2(pixely,pixelx) + twistAngle;
+			pixelx = Math.Cos(pixelAng) * pixelDist;
+			pixely = Math.Sin(pixelAng) * pixelDist;
+		}
+
+		var c = O.Sampler.Value.GetSample(frame,
+			(int)(swirlx + pixelx), (int)(swirly + pixely));
+		return c;
+	}
+}
---
+++ Plugin/Functions/Swirl/Options.cs
@@ -0,0 +1,89 @@
+using System.Drawing;
+using ImageFunctions.Core;
+using ImageFunctions.Core.Metrics;
+using ImageFunctions.Core.Samplers;
+using Rasberry.Cli;
+
+namespace ImageFunctions.Plugin.Functions.Swirl;
+
+public sealed class Options : IOptions
+{
+	public static Point? CenterPx;
+	public static PointF? CenterPp;
+	public static int? RadiusPx;
+	public static double? RadiusPp;
+	public static double Rotations;
+	public static bool CounterClockwise;
+	public static Lazy<ISampler> Sampler;
+	public static Lazy<IMetric> Metric;
+
+	public static void Usage(StringBuilder sb)
+	{
+		sb.ND(1,"Smears pixels in a circle around a point");
+		sb.ND(1,"-cx (number) (number)"      ,"Swirl center X and Y coordinate in pixels");
+		sb.ND(1,"-cp (number)[%] (number)[%]","Swirl center X and Y coordinate proportionally (default 50%,50%)");
+		sb.ND(1,"-rx (number)"               ,"Swirl radius in pixels");
+		sb.ND(1,"-rp (number)[%]"            ,"Swirl radius proportional to smallest image dimension (default 90%)");
+		sb.ND(1,"-s  (number)[%]"            ,"Number of rotations (default 0.9)");
+		sb.ND(1,"-ccw"                       ,"Rotate Counter-clockwise. (default is clockwise)");
+		sb.SamplerHelpLine();
+		sb.MetricHelpLine();
+	}
+
+	public static bool ParseArgs(string[] args, IRegister register)
+	{
+		var p = new ParseParams(args);
+
+		var parser = new ParseParams.Parser<double?>((string s, out double? p) => {
+			return ExtraParsers.TryParseNumberPercent(s,out p);
+		});
+
+		var pcx = p.Default("-cx",out int cx,out int cy);
+		if (pcx.IsInvalid()) {
+			return false;
+		}
+		else if (pcx.IsGood()) {
+			CenterPx = new Point(cx,cy);
+		}
+
+		var pcp = p.Default("-cp",out double? ppx,out double? ppy,
+			lefthPar: parser, rightPar: parser
+		);
+		if (pcp.IsInvalid()) {
+			return false;
+		}
+		else if (pcp.IsGood()) {
+			CenterPp = new PointF((float)ppx,(float)ppy);
+		}
+		//-cx and -cp are either/or options so set a default if neither are specified
+		if (CenterPx == null && CenterPp == null) {
+			CenterPp = new PointF(0.5f,0.5f);
+		}
+
+		if (p.Default("-rx",out RadiusPx).IsInvalid()) {
+			return false;
+		}
+		if (p.Default("-rp",out RadiusPp,par:parser).IsInvalid()) {
+			return false;
+		}
+		//-rx and -rp are either/or options so set a default if neither are specified
+		if (RadiusPx == null && RadiusPp == null) {
+			RadiusPp = 0.9;
+		}
+
+		if (p.Default("-s",out Rotations,0.9).IsInvalid()) {
+			return false;
+		}
+		if (p.Has("-ccw").IsGood()) {
+			CounterClockwise = true;
+		}
+		if (p.DefaultSampler(register, out Sampler).IsInvalid()) {
+			return false;
+		}
+		if (p.DefaultMetric(register, out Metric).IsInvalid()) {
+			return false;
+		}
+
+		return true;
+	}
+}
