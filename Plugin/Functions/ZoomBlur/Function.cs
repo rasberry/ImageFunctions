12c1c0ae9306c78f456917c3bb90bcad0b515373
rasberry <rasberry@hotmail.com>
2023-10-01 22:00:29 -0600

    added zoomblur function

---
+++ Plugin/Functions/ZoomBlur/Function.cs
@@ -0,0 +1,104 @@
+using System.Drawing;
+using ImageFunctions.Core;
+using Rasberry.Cli;
+using O = ImageFunctions.Plugin.Functions.ZoomBlur.Options;
+
+namespace ImageFunctions.Plugin.Functions.ZoomBlur;
+
+[InternalRegisterFunction(nameof(ZoomBlur))]
+public class Function : IFunction
+{
+	public void Usage(StringBuilder sb)
+	{
+		O.Usage(sb);
+	}
+
+	public bool Run(IRegister register, ILayers layers, string[] args)
+	{
+		if (layers == null) {
+			throw Squeal.ArgumentNull(nameof(layers));
+		}
+		if (!O.ParseArgs(args, register)) {
+			return false;
+		}
+
+		if (layers.Count < 1) {
+			Tell.LayerMustHaveAtLeast();
+			return false;
+		}
+
+		var source = layers.Last();
+		using var progress = new ProgressBar();
+		using var canvas = layers.NewCanvasFromLayers();
+		var bounds = canvas.Bounds();
+
+		double w2 = bounds.Width / 2.0;
+		double h2 = bounds.Height / 2.0;
+
+		if (O.CenterPx.HasValue) {
+			w2 = O.CenterPx.Value.X;
+			h2 = O.CenterPx.Value.Y;
+		}
+		else if (O.CenterRt.HasValue) {
+			w2 = bounds.Width * O.CenterRt.Value.X;
+			h2 = bounds.Height * O.CenterRt.Value.Y;
+		}
+
+		Tools.ThreadPixels(bounds, (x,y) => {
+			var d = source[x,y];
+			//Log.Debug($"pixel1 [{x},{y}] = ({d.R} {d.G} {d.B} {d.A})");
+			ColorRGBA nc = ZoomPixel(source,bounds,x,y,w2,h2);
+			int cy = y - bounds.Top;
+			int cx = x - bounds.Left;
+			//Log.Debug($"pixel2 [{cx},{cy}] = ({nc.R} {nc.G} {nc.B} {nc.A})");
+			canvas[cx,cy] = nc;
+		},progress);
+
+		source.CopyFrom(canvas, bounds);
+		return true;
+	}
+
+	ColorRGBA ZoomPixel(ICanvas frame, Rectangle rect, int x, int y,double cx, double cy)
+	{
+		var sampler = O.Sampler.Value;
+		double dist = O.Measurer.Value.Measure(x,y,cx,cy);
+		int idist = (int)Math.Ceiling(dist);
+
+		List<ColorRGBA> vector = new List<ColorRGBA>(idist);
+		double ang = Math.Atan2(y - cy, x - cx);
+		double sd = dist;
+		double ed = dist * O.ZoomAmount;
+
+		for (double d = sd; d < ed; d++)
+		{
+			double px = Math.Cos(ang) * d + cx;
+			double py = Math.Sin(ang) * d + cy;
+			ColorRGBA c = sampler.GetSample(frame,(int)px,(int)py);
+			vector.Add(c);
+		}
+
+		ColorRGBA avg;
+		int count = vector.Count;
+		if (count < 1) {
+			avg  = sampler.GetSample(frame,x,y);
+		}
+		else if (count == 1) {
+			avg = vector[0];
+		}
+		else {
+			double cr = 0, cg = 0, cb = 0, ca = 0;
+			foreach (ColorRGBA tpc in vector)
+			{
+				cr += tpc.R; cg += tpc.G; cb += tpc.B;
+				ca += tpc.A;
+			}
+			avg = new ColorRGBA(
+				cr / count,
+				cg / count,
+				cb / count,
+				ca / count
+			);
+		}
+		return avg;
+	}
+}
---
+++ Plugin/Functions/ZoomBlur/Options.cs
@@ -0,0 +1,71 @@
+using System.Drawing;
+using ImageFunctions.Core;
+using ImageFunctions.Core.Metrics;
+using ImageFunctions.Core.Samplers;
+using Rasberry.Cli;
+
+namespace ImageFunctions.Plugin.Functions.ZoomBlur;
+
+public sealed class Options : IOptions
+{
+	public static Lazy<ISampler> Sampler;
+	public static Lazy<IMetric> Measurer;
+	public static Point? CenterPx;
+	public static PointF? CenterRt;
+	public static double ZoomAmount;
+
+	public static void Usage(StringBuilder sb)
+	{
+		sb.ND(1,"Blends rays of pixels to produce a 'zoom' effect");
+		sb.ND(1,"-z  (number)[%]"             ,"Zoom amount (default 1.1)");
+		sb.ND(1,"-cc (number) (number)"       ,"Coordinates of zoom center in pixels");
+		sb.ND(1,"-cp (number)[%] (number)[%]" ,"Coordinates of zoom center by proportion (default 50% 50%)");
+		//sb.WL(" -oh"                        ,"Only zoom horizontally");
+		//sb.WL(" -ov"                        ,"Only zoom vertically");
+		sb.SamplerHelpLine();
+		sb.MetricHelpLine();
+	}
+
+	public static bool ParseArgs(string[] args, IRegister register)
+	{
+		var p = new ParseParams(args);
+		var parser = new ParseParams.Parser<double>((string n, out double p) => {
+			return ExtraParsers.TryParseNumberPercent(n, out p);
+		});
+
+		if (p.Default("-z",out ZoomAmount,1.1)
+			.BeGreaterThanZero("-z",ZoomAmount,true).IsInvalid()) {
+			return false;
+		}
+		var pcc = p.Default("-cc",out int cx, out int cy);
+		if (pcc.IsInvalid()) {
+			return false;
+		}
+		else if (pcc.IsGood()) {
+			CenterPx = new Point(cx,cy);
+		}
+		var pcp = p.Default("-cp",out double px, out double py,
+			lefthPar: parser, rightPar: parser
+		);
+		if (pcp.IsInvalid()) {
+			return false;
+		}
+		else if (pcp.IsGood()) {
+			CenterRt = new PointF((float)px,(float)py);
+		}
+
+		//-cc / -cp are either/or options. if neither are specified set the default
+		if (CenterPx == null && CenterRt == null) {
+			CenterRt = new PointF(0.5f,0.5f);
+		}
+
+		if (p.DefaultSampler(register, out Sampler).IsInvalid()) {
+			return false;
+		}
+		if (p.DefaultMetric(register, out Measurer).IsInvalid()) {
+			return false;
+		}
+
+		return true;
+	}
+}
