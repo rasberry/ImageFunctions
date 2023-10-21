9589af8f4a96e46b8fc5565ad77080a0766f31bb
rasberry <rasberry@hotmail.com>
2023-10-21 17:29:22 -0600

    added TestDerivatives

---
+++ Test/Plugin/Functions/TestDerivatives.cs
@@ -0,0 +1,50 @@
+using ImageFunctions.Core;
+
+namespace ImageFunctions.Test;
+
+[TestClass]
+public class TestDerivatives : AbstractFunctionTest
+{
+	const string MyName = nameof(Plugin.Functions.Derivatives);
+	public override string FunctionName { get { return MyName; }}
+
+	[TestMethod]
+	[DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
+	public void Test(TestFunctionInfo info)
+	{
+		using var layers = new Layers();
+		info.Layers = layers;
+		RunFunctionAndCompare(info, 60.0);
+	}
+
+	public static IEnumerable<object[]> GetData()
+	{
+		foreach(var imgName in GetImageNames()) {
+			foreach(var info in GetFunctionInfo(imgName)) {
+				yield return new object[] { info };
+			}
+		}
+	}
+
+	public static IEnumerable<TestFunctionInfo> GetFunctionInfo(string startImg)
+	{
+		yield return CreateTestInfo(1, startImg, new string[0]);
+		yield return CreateTestInfo(2, startImg, new string[] { "-g" });
+		yield return CreateTestInfo(3, startImg, new string[] { "-a" });
+	}
+
+	static TestFunctionInfo CreateTestInfo(int index, string startImg, string[] args)
+	{
+		return new TestFunctionInfo {
+			Args = args,
+			OutName = $"{MyName}-{startImg}-{index}",
+			ImageNames = new[] { startImg }
+		};
+	}
+
+	public static IEnumerable<string> GetImageNames()
+	{
+		var list = new string[] { "fractal","handle" };
+		return list;
+	}
+}
